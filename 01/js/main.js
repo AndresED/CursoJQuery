912044239eeaf0f6670891fcb7e8b2c1004e399e
AndresED <andres30xed@gmail.com>
2018-05-19 08:57:58 -0500

    Agregando y quitando clases y propiedades css

---
+++ 01/js/main.js
@@ -0,0 +1,16 @@
+/*jQuery('ul.list1').addClass('textoVerde');
+jQuery('ul.list2').addClass('textoRojo');
+jQuery('ul.list3').addClass('textoAmarillo');
+jQuery('ul#list4').addClass('textoAzul');
+jQuery('ul#list4').css('font-size','50px');
+jQuery('ul').css('font-weight','bold');*/
+
+
+$(document).ready(function(){
+  $('ul.list1').addClass('textoVerde');
+  $('ul.list2').addClass('textoRojo');
+  $('ul.list3').addClass('textoAmarillo');
+  $('ul#list4').addClass('textoAzul');
+  $('ul#list4').css('font-size','50px');
+  $('ul').css('font-weight','bold');
+});
